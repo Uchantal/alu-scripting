cb860671e06ae406314546ecc1858d51b2b83d8a
Uchantal <u.chantal@alustudent.com>
2024-07-01 20:09:42 +0000

    changed

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""
-Return the number of subscribers
-from any subreddit given
-"""
+"""DOC"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Myapi-app'}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        value = r.json()
-        return value['data']['subscribers']
+    """DOC"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+
+    header = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url,
+                            headers=header
+                            )
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        subs = data['subscribers']
+        return subs
     return 0
