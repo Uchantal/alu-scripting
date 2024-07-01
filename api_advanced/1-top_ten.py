c96d739b083c2d46295504fd41bc86dc582e4164
Uchantal <u.chantal@alustudent.com>
2024-07-01 20:53:57 +0000

    stressed me

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """
-Finding hot top 10 post
+function that queries the 'Reddit API'
+and prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Myapi-app'}
-
-    r = requests.get(url+'?limit=10', headers=headers)
-    if r.status_code == 200:
-        value = r.json()
-        datas = value['data']['children']
-        for each in datas:
-            title = each['data']['title']
-            print(title)
-    else:
-        print('None')
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
